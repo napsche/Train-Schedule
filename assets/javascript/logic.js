fa171e0e187aaa0059f9ecfc17202ff2f54a49b0
Nicole Apsche <napsche@kgtiger.com>
2020-05-23 16:19:25 -0400

    linking and adding firebase

---
+++ assets/javascript/logic.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyChrQ-OMFZSG4PQgvFPbHLvp7K6SiF9fY0",
+    authDomain: "train-schedule-ace61.firebaseapp.com",
+    databaseURL: "https://train-schedule-ace61.firebaseio.com",
+    projectId: "train-schedule-ace61",
+    storageBucket: "train-schedule-ace61.appspot.com",
+    messagingSenderId: "84510197054",
+    appId: "1:84510197054:web:6185efa28ef6fa167f186f"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
